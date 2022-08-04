ab42b87dac37ffa8953ba56ac25751959933ecf6
Paul Wögerer <paul.woegerer@oracle.com>
2022-08-04 15:32:47 +0200

    Use JNIRuntimeAccess API were appropriate

--- substratevm/src/com.oracle.svm.graal.hotspot.libgraal/src/com/oracle/svm/graal/hotspot/libgraal/LibGraalFeature.java
+++ substratevm/src/com.oracle.svm.graal.hotspot.libgraal/src/com/oracle/svm/graal/hotspot/libgraal/LibGraalFeature.java
@@ -103,8 +103,8 @@ import org.graalvm.nativeimage.StackValue;
 import org.graalvm.nativeimage.VMRuntime;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.FieldValueTransformer;
+import org.graalvm.nativeimage.hosted.RuntimeJNIAccess;
 import org.graalvm.nativeimage.hosted.RuntimeReflection;
-import org.graalvm.nativeimage.impl.ConfigurationCondition;
 import org.graalvm.word.LocationIdentity;
 import org.graalvm.word.Pointer;
 import org.graalvm.word.WordFactory;
@@ -127,7 +127,6 @@ import com.oracle.svm.core.graal.meta.RuntimeConfiguration;
 import com.oracle.svm.core.graal.snippets.NodeLoweringProvider;
 import com.oracle.svm.core.heap.GCCause;
 import com.oracle.svm.core.heap.Heap;
-import com.oracle.svm.core.jni.JNIRuntimeAccess;
 import com.oracle.svm.core.log.FunctionPointerLogHandler;
 import com.oracle.svm.core.option.HostedOptionKey;
 import com.oracle.svm.core.option.RuntimeOptionKey;
@@ -215,10 +214,9 @@ public class LibGraalFeature implements com.oracle.svm.core.graal.InternalFeatur
 
     @Override
     public void duringSetup(DuringSetupAccess access) {
-        JNIRuntimeAccess.JNIRuntimeAccessibilitySupport registry = ImageSingletons.lookup(JNIRuntimeAccess.JNIRuntimeAccessibilitySupport.class);
         ImageClassLoader imageClassLoader = ((DuringSetupAccessImpl) access).getImageClassLoader();
-        registerJNIConfiguration(registry, imageClassLoader);
 
+        registerJNIConfiguration(imageClassLoader);
         EconomicMap<String, OptionDescriptor> descriptors = EconomicMap.create();
         for (Class<? extends OptionDescriptors> optionsClass : imageClassLoader.findSubclasses(OptionDescriptors.class, false)) {
             if (!Modifier.isAbstract(optionsClass.getModifiers()) && !OptionDescriptorsMap.class.isAssignableFrom(optionsClass)) {
@@ -348,10 +346,9 @@ public class LibGraalFeature implements com.oracle.svm.core.graal.InternalFeatur
         }
     }
 
-    private static void registerJNIConfiguration(JNIRuntimeAccess.JNIRuntimeAccessibilitySupport registry, ImageClassLoader loader) {
+    private static void registerJNIConfiguration(ImageClassLoader loader) {
         try (JNIConfigSource source = new JNIConfigSource(loader)) {
             Map<String, Class<?>> classes = new HashMap<>();
-            ConfigurationCondition condition = ConfigurationCondition.alwaysTrue();
             for (String line : source.lines) {
                 source.lineNo++;
                 String[] tokens = line.split(" ");
@@ -360,8 +357,8 @@ public class LibGraalFeature implements com.oracle.svm.core.graal.InternalFeatur
                 Class<?> clazz = classes.get(className);
                 if (clazz == null) {
                     clazz = source.findClass(className);
-                    registry.register(condition, clazz);
-                    registry.register(condition, Array.newInstance(clazz, 0).getClass());
+                    RuntimeJNIAccess.register(clazz);
+                    RuntimeJNIAccess.register(Array.newInstance(clazz, 0).getClass());
                     classes.put(className, clazz);
                 }
 
@@ -370,7 +367,7 @@ public class LibGraalFeature implements com.oracle.svm.core.graal.InternalFeatur
                         source.check(tokens.length == 4, "Expected 4 tokens for a field");
                         String fieldName = tokens[2];
                         try {
-                            registry.register(condition, false, clazz.getDeclaredField(fieldName));
+                            RuntimeJNIAccess.register(clazz.getDeclaredField(fieldName));
                         } catch (NoSuchFieldException e) {
                             throw source.error("Field %s.%s not found", clazz.getTypeName(), fieldName);
                         } catch (NoClassDefFoundError e) {
@@ -389,7 +386,7 @@ public class LibGraalFeature implements com.oracle.svm.core.graal.InternalFeatur
                         try {
                             if ("<init>".equals(methodName)) {
                                 Constructor<?> cons = clazz.getDeclaredConstructor(parameters);
-                                registry.register(condition, false, cons);
+                                RuntimeJNIAccess.register(cons);
                                 if (Throwable.class.isAssignableFrom(clazz) && !Modifier.isAbstract(clazz.getModifiers())) {
                                     if (usedInTranslatedException(parameters)) {
                                         RuntimeReflection.register(clazz);
@@ -397,7 +394,7 @@ public class LibGraalFeature implements com.oracle.svm.core.graal.InternalFeatur
                                     }
                                 }
                             } else {
-                                registry.register(condition, false, clazz.getDeclaredMethod(methodName, parameters));
+                                RuntimeJNIAccess.register(clazz.getDeclaredMethod(methodName, parameters));
                             }
                         } catch (NoSuchMethodException e) {
                             throw source.error("Method %s.%s%s not found: %s", clazz.getTypeName(), methodName, descriptor, e);
